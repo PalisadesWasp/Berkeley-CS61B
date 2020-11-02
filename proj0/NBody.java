c5d9d03c1e7688e34f525e440599cc264bf3a21c
ortanul <ortanul@gmail.com>
2020-11-02 16:45:08 +0800

    comments added

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -1,11 +1,30 @@
+/**
+ * Actually runs simulation of an universe specified in one of the data files
+ * command:
+ * javac NBody.java
+ * java NBody 157788000.0 25000.0 data/planets.txt
+ */
+
 public class NBody {
+    /**
+     * Given a file name, returns a double corresponding to the radius of the universe in the file
+     * @param filePath: file path and name
+     */
     public static double readRadius(String filePath) {
         In in = new In(filePath);
         int num = in.readInt();
         double radius = in.readDouble();
         return radius;
+        /** Compile and run with: */
+        // javac NBody.java TestReadRadius.java
+        // java TestReadRadius
     }
 
+    /**
+     * Given a file name, returnS an array of Planets corresponding to the planets in the file
+     * @param filePath: file path and name
+     * @return
+     */
     public static Planet[] readPlanets(String filePath) {
         In in = new In(filePath);
         int numP = in.readInt();
@@ -18,50 +37,60 @@ public class NBody {
             double yV = in.readDouble();
             double m = in.readDouble();
             String img = in.readString();
-            Planet p = new Planet(xP, yP, xV, yV, m, img);
-            planets[i] = p;
+            planets[i] = new Planet(xP, yP, xV, yV, m, img);
         }
         return planets;
+        /** Compile and run with: */
+        // javac Planet.java NBody.java TestReadPlanets.java
+        // java TestReadPlanets
     }
 
     public static void main(String[] args) {
-        double T = Double.parseDouble(args[0]);
-        double dt = Double.parseDouble(args[1]);
-        String filename = args[2];
-        double radius = readRadius(filename);
+        /** Read command line arguments and collects all needed input */
+        double T = Double.parseDouble(args[0]); // time to stop simulation
+        double dt = Double.parseDouble(args[1]); // small period of time
+        String filename = args[2]; // file name and path of the data file w/ planet information
+        double radius = readRadius(filename); // radius of the universe
         Planet[] planets = readPlanets(filename);
         double currTime = 0;
         int numP = planets.length;
 
+        /** Draw the background and the planets */
         StdDraw.setScale(-radius, radius);
         StdDraw.clear();
         StdDraw.picture(0, 0, "images/starfield.jpg");
         for (Planet p : planets) {
             p.draw();
         }
+        /** Compile and run with: */
+        // javac NBody.java
+        // java NBody 157788000.0 25000.0 data/planets.txt
 
-        StdDraw.enableDoubleBuffering();
-        StdDraw.show();
+        StdDraw.enableDoubleBuffering(); // Graphics technique to prevent flickering in the animation
+        StdDraw.show(); // Drawing get copied from the offscreen canvas to the onscreen canvas
 
         while (currTime < T) {
             double[] xForces = new double[numP];
             double[] yForces = new double[numP];
+            /** Calculate the net x and y forces for each planet */
             for (int i = 0; i < numP; i++) {
                 xForces[i] = planets[i].calcNetForceExertedByX(planets);
                 yForces[i] = planets[i].calcNetForceExertedByY(planets);
             }
+            /** Update each planet’s position, velocity, and acceleration */
             for (int i = 0; i < numP; i++) {
                 planets[i].update(dt, xForces[i], yForces[i]);
             }
-            StdDraw.picture(0, 0, "images/starfield.jpg");
+            StdDraw.picture(0, 0, "images/starfield.jpg"); // Draw the background image
             for (Planet p : planets) {
-                p.draw();
+                p.draw(); // Draw all of the planets
             }
-            StdDraw.show();
-            StdDraw.pause(10);
+            StdDraw.show(); // Show the offscreen buffer
+            StdDraw.pause(10); // Pause the animation for 10 milliseconds
             currTime += dt;
         }
 
+        /** Print out the final state of the universe when the simulation is over */
         StdOut.printf("%d\n", planets.length);
         StdOut.printf("%.2e\n", radius);
         for (int i = 0; i < planets.length; i++) {
--- proj0/Planet.java
+++ proj0/Planet.java
@@ -1,13 +1,22 @@
 public class Planet {
-    public double xxPos;
-    public double yyPos;
-    public double xxVel;
-    public double yyVel;
-    public double mass;
-    public String imgFileName;
+    public double xxPos; // current x position
+    public double yyPos; // current y position
+    public double xxVel; // current velocity in the x direction
+    public double yyVel; // current velocity in the y direction
+    public double mass; // mass of the planet
+    public String imgFileName; // the name of the file that corresponds to the image that depicts the planet
 
-    public static final double gCons = 6.67e-11;
+    private static final double gCons = 6.67e-11; // gravitational constant
 
+    /**
+     * Constructor that initializes an instance of the Planet class w/ given parameters
+     @param xP: current x position
+     @param yP: current y position
+     @param xV: current velocity in the x direction
+     @param yV: current velocity in the y direction
+     @param m: mass of the planet
+     @param img: the name of the file that corresponds to the image that depicts the planet
+     */
     public Planet(double xP, double yP, double xV, double yV, double m, String img) {
         xxPos = xP;
         yyPos = yP;
@@ -15,8 +24,13 @@ public class Planet {
         yyVel = yV;
         mass = m;
         imgFileName = img;
+        /** Below can replaces all above */
+        // this.Planet(p.xxPos, p.yyPos, p.xxVel, p.yyVel, p.mass, p.imgFileName);
     }
 
+    /**
+     * Constructor that takes in a Planet object and initializes an identical Planet object
+     */
     public Planet(Planet p) {
         this.xxPos = p.xxPos;
         this.yyPos = p.yyPos;
@@ -26,24 +40,49 @@ public class Planet {
         this.imgFileName = p.imgFileName;
     }
 
+    /**
+     * Calculates the distance between two Planets
+     */
     public double calcDistance(Planet p) {
         return Math.sqrt(Math.pow((this.xxPos - p.xxPos), 2) + Math.pow((this.yyPos - p.yyPos), 2));
+        /** Compile and run with: */
+        // javac Planet.java TestCalcDistance.java
+        // java TestCalcDistance
     }
 
+    /**
+     * Calculates the force exerted on this Planet by the given Planet
+     */
     public double calcForceExertedBy(Planet p) {
         return gCons * this.mass * p.mass / Math.pow(this.calcDistance(p), 2);
+        /** Compile and run with: */
+        // javac Planet.java TestCalcForceExertedBy.java
+        // java TestCalcForceExertedBy
     }
 
+    /**
+     * Calculates the force exerted in the X direction
+     */
     public double calcForceExertedByX(Planet p) {
         double xCoorDiff = p.xxPos - this.xxPos;
         return this.calcForceExertedBy(p) * xCoorDiff / this.calcDistance(p);
     }
 
+    /**
+     * Calculates the force exerted in the Y direction
+     */
     public double calcForceExertedByY(Planet p) {
         double yCoorDiff = p.yyPos - this.yyPos;
         return this.calcForceExertedBy(p) * yCoorDiff / this.calcDistance(p);
+        /** Compile and run with: */
+        // javac Planet.java TestCalcForceExertedByXY.java
+        // java TestCalcForceExertedByXY
     }
 
+    /**
+     * Take in an array of Planets and calculate the net X force exerted by all planets
+     * in that array upon the current Planet
+     */
     public double calcNetForceExertedByX(Planet[] ps) {
         double xNetForce = 0;
         for (Planet p : ps) {
@@ -54,6 +93,10 @@ public class Planet {
         return xNetForce;
     }
 
+    /**
+     * Take in an array of Planets and calculate the net Y force exerted by all planets
+     * in that array upon the current Planet
+     */
     public double calcNetForceExertedByY(Planet[] ps) {
         double yNetForce = 0;
         for (Planet p : ps) {
@@ -62,8 +105,18 @@ public class Planet {
             }
         }
         return yNetForce;
+        /** Compile and run with: */
+        // javac Planet.java TestCalcNetForceExertedByXY.java
+        // java TestCalcNetForceExertedByXY
     }
 
+    /**
+     * Determines how much the forces exerted on the planet will cause that planet to accelerate,
+     * and the resulting change in the planet’s velocity and position in a small period of time dt
+     * @param dt: small period of time
+     * @param fX: net force in X direction
+     * @param fY: net force in Y direction
+     */
     public void update(double dt, double fX, double fY) {
         double xAcc = fX / mass;
         double yAcc = fY / mass;
@@ -71,6 +124,9 @@ public class Planet {
         yyVel += dt * yAcc;
         xxPos += dt * xxVel;
         yyPos += dt * yyVel;
+        /** Compile and run with: */
+        // javac Planet.java TestUpdate.java
+        // java TestUpdate
     }
 
     public void draw() {
