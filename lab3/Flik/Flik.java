4b1ab13a95610c6c16f8fd2dda479d09e57c78ac
wenzhexu <ortanul@gmail.com>
2022-03-24 19:39:31 +0800

    modified function isSameNumber

--- lab3/Flik/Flik.java
+++ lab3/Flik/Flik.java
@@ -1,6 +1,10 @@
 /** An Integer tester created by Flik Enterprises. */
 public class Flik {
     public static boolean isSameNumber(Integer a, Integer b) {
-        return a == b;
+        /* For Integer objects, if the value is between -128 and 127, then == checks if the values are the same */
+        return a == b; // i is 128
+
+        /* When the value is > 127 or < -128, == checks if the variables refer to the same object reference (address) */
+        //return a.equals(b); // uses a.equals(b) instead (i is 500)
     }
 }
